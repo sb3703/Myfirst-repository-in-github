69b804307bbb63c9bd5cb1729f624926e6def6d8
Srikar Bajjuri <144845772+sb3703@users.noreply.github.com>
2023-09-13 01:28:04 +0530

    Add files via upload

---
+++ Myfirstprogramfile.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+  //just started with github
+  //13th sept 2023
+  printf("\n\n\nHey guys my name is Srikar Bajjuri \nstudying in VIT Vellore,\ncurrently pursuing my Mtech integrated in CSE \n\n\n");
+}
